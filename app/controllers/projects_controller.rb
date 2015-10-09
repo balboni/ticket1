702e9a19f4e3c1b65a5bd4b8853f1cb486664dd9
balboni <brian@brianbalboni.com>
2015-10-09 16:02:03 -0500

    complete p92

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,6 +12,18 @@ class ProjectsController < ApplicationController
 		@project = Project.find(params[:id])
 	end
 
+	def edit
+		@project = Project.find(params[:id])
+	end
+
+	def update
+		@project = Project.find(params[:id])
+		@project.update(project_params)
+
+		flash[:notice] = "Project has been updated."
+		redirect_to @project
+	end
+
 
 	def create
 		@project = Project.new(project_params)
